2ec0dc60833525339abc6834edc14f74bb598dc6
Manveet Mandal <manveetm3@gmail.com>
2024-11-26 01:25:17 -0700

    feat: Move data code into utils file

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-import torch
-from torchvision import datasets
-from torchvision.transforms import v2
+import utils.data as data
 
-batchSize = 128
-transform = v2.Compose([
-    v2.ToImage(),                           # Convert to Tensor
-    v2.ToDtype(torch.float32, scale=True),  # Scale pixel values to [0, 1]
-    v2.Lambda(lambda x: x.view(-1))         # Flatten image
-])
-
-# Load the training data
-trainData = datasets.FashionMNIST(
-    './data/', download=True, train=True, transform=transform
-    )
-
-# Load the test data
-testData = datasets.MNIST(
-    './data/', download=True, train=False, transform=transform
-)
-
-# Create dataloaders
-trainLoader = torch.utils.data.DataLoader(
-    trainData, batch_size=batchSize, shuffle=True
-)
-
-testLoader = torch.utils.data.DataLoader(
-    testData, batch_size=batchSize, shuffle=False
-)
+print(data.batchSize)
